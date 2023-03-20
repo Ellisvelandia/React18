21872850efbe2a57aed93fc8fde5bb45312b1a89
Ellisvelandia <eyis619l@gmail.com>
2023-03-20 15:44:01 -0500

    react18

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>First component</h1>
+      <p>this is my message</p>
+    </div>
+  );
+};
+
+export default App;
